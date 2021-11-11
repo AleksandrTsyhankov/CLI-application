cb23ee9ab15172efccff5c23ffbfcfbea66788cb
AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
2021-11-11 19:08:19 +0200

    1hw separate functions from contacts.js into files for each func

--- contacts.js
+++ contacts.js
@@ -1,52 +1,8 @@
-const fs = require('fs/promises');
-const path = require('path');
-const crypto = require('crypto');
-
-
-const contactsPath = path.join(__dirname, 'db/contacts.json');
- 
-const readData = async () => {
-    const result = await fs.readFile(contactsPath, 'utf8');
-    return JSON.parse(result);
+const contactsArr = {
+    listContacts: require('./controllers/contacts/listContacts'),
+    getContactById: require('./controllers/contacts/getContactById'),
+    removeContact: require('./controllers/contacts/removeContact'),
+    addContact: require('./controllers/contacts/addContact'),
 }
 
-const listContacts = async() => {
-    return await readData();
-}
-
-const getContactById = async (contactId) => {
-    const contacts = await readData();
-    const [result] = contacts.filter((contact) => JSON.stringify(contact.id) === contactId);
-    return result;
-}
-
-const removeContact = async (contactId) => {
-    const allContacts = await readData();
-    const filteredContacts = allContacts.filter(contact => JSON.stringify(contact.id) !== contactId);
-    const removedContact = allContacts.filter(contact => JSON.stringify(contact.id) === contactId);
-
-    await fs.writeFile(
-        contactsPath,
-        JSON.stringify(filteredContacts, null, 2),
-    );
-    return removedContact;
-}
-
-const addContact = async (name, email, phone) => {
-    const contacts = await readData();
-    const newContact = { id: crypto.randomUUID(), name, email, phone };
-    contacts.push(newContact);
-
-    await fs.writeFile(
-        contactsPath,
-        JSON.stringify(contacts, null, 2),
-    )
-    return newContact;
-};
-
-module.exports = {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact,
-};
+module.exports = contactsArr;
---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,25 @@
+const fs = require('fs/promises');
+const path = require('path');
+const crypto = require('crypto');
+
+
+const contactsPath = path.join(__dirname, '../../db/contacts.json');
+ 
+const readData = async () => {
+    const result = await fs.readFile(contactsPath, 'utf8');
+    return JSON.parse(result);
+}
+
+const addContact = async (name, email, phone) => {
+    const contacts = await readData();
+    const newContact = { id: crypto.randomUUID(), name, email, phone };
+    contacts.push(newContact);
+
+    await fs.writeFile(
+        contactsPath,
+        JSON.stringify(contacts, null, 2),
+    )
+    return newContact;
+};
+
+module.exports = addContact;
